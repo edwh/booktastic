8cab3555b6a0f14b41fb2739303dc80840041633
Edward Hibbert <edward@ehibbert.org.uk>
2020-04-27 12:36:52 +0100

    Identify spines

--- identifySpines.go
+++ identifySpines.go
@@ -3,25 +3,38 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"math"
 	"regexp"
 	"strings"
 )
+
+const PRUNE_SMALL_TEXT = 4
 
 // Google OCR returns an array of these.
 type OCRFragment struct {
 	Locale       string
 	Description  string
-	BoundingPoly interface{}
+	BoundingPoly BoundingPoly
+	SpineIndex   int
+}
+
+type BoundingPoly struct {
+	Vertices []Vertices
+}
+
+type Vertices struct {
+	X int
+	Y int
 }
 
 type Spine struct {
-	original string // The original text returned from the OCR.
-	spine    string // The current working text
-	author   string // Identified author
-	title    string // Identified subject
+	Spine  string  // The current working text
+	Author *string // Identified author
+	Title  *string // Identified subject
 }
 
 func GetLinesAndFragments(str string) ([]string, []OCRFragment) {
+	// TODO Need default values of X and Y as Google omits if 0.
 	var m []OCRFragment
 	json.Unmarshal([]byte(str), &m)
 
@@ -34,6 +47,71 @@ func GetLinesAndFragments(str string) ([]string, []OCRFragment) {
 	fragments := m[1:]
 
 	return lines, fragments
+}
+
+func MaxDimension(poly BoundingPoly) int {
+	vertices := poly.Vertices
+
+	x := math.Abs(float64(vertices[0].X) - float64(vertices[3].X))
+	y := math.Abs(float64(vertices[0].Y) - float64(vertices[3].Y))
+
+	return int(math.Max(x, y))
+}
+
+func PruneSmallText(lines []string, fragments []OCRFragment, ratio int) int {
+	// Very small text on spines is likely to be publishers, ISBN numbers, stuff we've read from the front at an angle,
+	// or otherwise junk.  So let's identify the typical letter height, and prune out stuff that's much smaller.
+	total := 0
+	pruned := 0
+
+	for _, fragment := range fragments {
+		total += MaxDimension(fragment.BoundingPoly)
+	}
+
+	mean := total / len(fragments)
+
+	log.Printf("Mean max %d", mean)
+
+	newlines := []string{}
+	newfragments := []OCRFragment{}
+	fragindex := 0
+
+	for lineindex, line := range lines {
+		linewords := strings.Split(line, " ")
+		newlinewords := []string{}
+
+		for _, word := range linewords {
+			if len(word) > 0 {
+				log.Printf("Consider %s line %d, fragment %d vs %d", word, lineindex, fragindex, len(fragments))
+
+				fragment := fragments[fragindex]
+				if word != fragment.Description {
+					log.Printf("ERROR: Mismatch spine/fragment %s vs %s", word, fragment.Description)
+					panic("Mismatch spine/fragment")
+				} else {
+					thismax := MaxDimension(fragment.BoundingPoly)
+					log.Printf("Max %d vs %d", thismax, mean)
+
+					if thismax*ratio < mean {
+						log.Printf("Prune small text %s size %d vs %d at %d", fragment.Description, thismax, mean, fragindex)
+						pruned++
+					} else {
+						newlinewords = append(newlinewords, fragment.Description)
+						newfragments = append(newfragments, fragment)
+					}
+
+					fragindex++
+				}
+			}
+		}
+
+		newlines = append(newlines, strings.Join(newlinewords, " "))
+	}
+
+	lines = newlines
+	fragments = newfragments
+
+	return pruned
 }
 
 func CleanOCR(str string) string {
@@ -60,7 +138,7 @@ func CleanOCR(str string) string {
 	newstr = regexp.MustCompile(`\s#\s`).ReplaceAllString(newstr, "")
 
 	// Quotes confuse matters.
-	newstr = regexp.MustCompile(`"`).ReplaceAllString(newstr, "")
+	newstr = regexp.MustCompile(`"|'`).ReplaceAllString(newstr, "")
 
 	// Collapse multiple spaces.
 	newstr = regexp.MustCompile(`\s+`).ReplaceAllString(newstr, " ")
@@ -72,4 +150,44 @@ func CleanOCR(str string) string {
 	}
 
 	return newstr
+}
+
+func AddSpineIndex(lines []string, fragments []OCRFragment) {
+	fragindex := 0
+
+	for spineindex, line := range lines {
+		words := strings.Split(strings.TrimSpace(line), " ")
+
+		for _, word := range words {
+			if len(word) > 0 {
+				if word == fragments[fragindex].Description {
+					fragments[fragindex].SpineIndex = spineindex
+					fragindex++
+				} else {
+					panic("Mismatch adding spine index")
+				}
+			}
+		}
+	}
+}
+
+func IdentifySpines(lines []string, fragments []OCRFragment) []Spine {
+	spines := []Spine{}
+
+	PruneSmallText(lines, fragments, PRUNE_SMALL_TEXT)
+	AddSpineIndex(lines, fragments)
+
+	for _, line := range lines {
+		cleaned := CleanOCR(line)
+
+		if len(cleaned) > 0 {
+			spines = append(spines, Spine{
+				Spine:  line,
+				Author: nil,
+				Title:  nil,
+			})
+		}
+	}
+
+	return spines
 }
--- identifySpines_test.go
+++ identifySpines_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"log"
 	"testing"
 )
 
@@ -16,4 +17,21 @@ func TestDimension(t *testing.T) {
 	lines, fragments := GetLinesAndFragments(SAMPLE)
 	assert.Equal(t, "PMC", lines[0])
 	assert.Equal(t, "PMC", fragments[0].Description)
+	assert.Equal(t, 333, fragments[0].BoundingPoly.Vertices[0].X)
+	assert.Equal(t, 36, MaxDimension(fragments[0].BoundingPoly))
+}
+
+func TestPruneSmallText(t *testing.T) {
+	lines, fragments := GetLinesAndFragments(SAMPLE)
+
+	// Force some pruning.
+	assert.Equal(t, 83, PruneSmallText(lines, fragments, 1))
+	assert.Equal(t, 1, PruneSmallText(lines, fragments, PRUNE_SMALL_TEXT))
+}
+
+func TestIdentifySpines(t *testing.T) {
+	lines, fragments := GetLinesAndFragments(SAMPLE)
+	spines := IdentifySpines(lines, fragments)
+	log.Printf("Spine %+v", spines[0])
+	assert.Equal(t, "PMC", spines[0].Spine)
 }
